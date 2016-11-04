09a5fc3040718e7ed8cbdc697cb998f8fed3fd5a
divyanshu  rawat <divyanshu.r46956@gmail.com>
2016-11-05 02:42:59 +0530

    added functionality for Disable/Ban User Accounts !!!

--- Core/init.php
+++ Core/init.php
@@ -15,6 +15,16 @@ if(logged_in() === true)
 {
 	$session_user_id = $_SESSION['user_id'];
 	$user_data = user_data($connect,$session_user_id,'user_id','username','password','first_name','last_name','email');
+
+	if(user_active($connect,$user_data['username']) == false)
+	{
+		session_destroy();
+		header('Location: index.php');
+		exit();
+	}
+
+	# added functionality for Disable/Ban User Accounts !!!
+
 }
 
 ?>
