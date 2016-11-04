4bf71121849d0fc1610ceba664931e8d39744525
divyanshu  rawat <divyanshu.r46956@gmail.com>
2016-11-05 03:22:01 +0530

    code improved !!!

--- Templates/Widgets/user_count.php
+++ Templates/Widgets/user_count.php
@@ -3,6 +3,10 @@
 	<h2>Users</h2>
 
 	<div class="inner">
-		we current,have <?php echo user_count($connect); ?> registered users.
+	<?php
+		$user_count = user_count($connect);
+		$suffix = ($user_count !== 1) ? 's':'';
+	?>
+		we current,have <?php echo $user_count; ?> registered user<?php echo $suffix?>.
 	</div>
 </div>
