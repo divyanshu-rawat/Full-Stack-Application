f6d88cba87a2564dba04630b7195551cbd78cee1
divyanshu  rawat <divyanshu.r46956@gmail.com>
2016-11-07 16:07:39 +0530

    creating activate.php

---
+++ activate.php
@@ -0,0 +1,12 @@
+
+<?php 
+		// error_reporting(0);
+        include 'core\init.php';
+        logged_in_redirect();
+        include 'Templates\Overall_footer_Header\header.php';
+    	include 'Templates\Overall_footer_Header\footer.php';
+
+
+    	
+
+?>
